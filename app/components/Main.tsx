5aec63169263d15c2b0c6c835dd433085d2b4cd4
izmaelmag <caspian.seagull@gmail.com>
2021-12-02 03:13:26 +0300

    Use Timer and DonationsCount components

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,20 +1,27 @@
-import React, { useEffect } from "react";
-import { Container, Label, GlobalStyle } from "./UI";
-import useSocketData from '../hooks/useSocketData'
+import React, { useEffect, useMemo } from "react";
+import { Container, GlobalStyle, RunnerInfo } from "./UI";
+import useSocketData from "../hooks/useSocketData";
+import Timer from './Timer'
+import DonationCounter from "./DonationCounter";
 
 const Main = () => {
-  const [data, isOpen] = useSocketData(`ws://localhost:8787`)
+  const [data, isOpen] = useSocketData(`ws://localhost:8787`);
 
   useEffect(() => {
-    console.log(data)
-  }, [data])
+    console.log(data);
+  }, [data]);
 
   return (
     <React.Fragment>
       <GlobalStyle />
 
       <Container>
-        <Label>Header</Label>
+        <Timer seconds={data.seconds}/>
+        <DonationCounter current={data.donates} goal={data.donationGoal} />
+
+        {data.runners.map(runner => (
+          <RunnerInfo><b>{runner.name}:</b> {runner.currentDistance.toFixed(2)}km</RunnerInfo>
+        ))}
       </Container>
     </React.Fragment>
   );
